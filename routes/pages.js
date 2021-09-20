282dd56af3597b0d20cf482dcb8924e915c15e96
Benilde <benilde@SDA-1012-08.local>
2021-09-20 22:48:55 +0800

    Updated Project - Pages, Registration, Log-in

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const router = express.Router();
 
+//start the database
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 router.get('/', (req,res)=>{
     res.render('index');
@@ -15,9 +22,34 @@ router.get('/login', (req,res)=>{
     res.render('login');
 });
 
-router.get('/customer', (req,res)=>{
-    res.render('customer');
-});
+router.get('/customer', function (req, res) {
+   res.render('customer');
+  });
+
+
+router.get('/petreg', (req, res) =>{
+   res.render('petreg');
+  });
+
+
+  router.get('/addPetreg', (req,res)=>{
+      res.render('addPetreg');
+  })
+
+
+//STATIC PAGES
+router.get('/missionVision', (req,res)=>{
+    res.render('missionVision');
+})
+
+
+router.get('/theTeam', (req,res)=>{
+    res.render('theTeam');
+})
+
+router.get('/listServices', (req,res)=>{
+    res.render('listServices');
+})
 
 module.exports = router;
 
